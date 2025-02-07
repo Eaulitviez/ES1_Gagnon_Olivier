695859dd0237c3b40c66d385ae16973b8cf68015
Eaulitviez <2077834@cegepmontpetit.ca>
2025-02-07 12:00:28 -0500

    script

---
+++ Assets/Scripts/JeuxBille.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class JeuxBille : MonoBehaviour
+
+
+
+{
+    [SerializeReference] private GameObject _plateforme;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+       public void DetruirePlateforme()
+        {
+            _plateforme.SetActive(false);
+        }
+        
+
+}
